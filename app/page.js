d1ad6c5716728adb727f6cae9c654898dba6e389
WILDCATZWEB3 <dydxcryptocoin@gmail.com>
2025-02-15 14:24:13 +0100

    Restored app directory and removed pages

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to G Learning Network</h1>
+      <p>Your platform for learning Web3, blockchain, and content writing.</p>
+    </main>
+  );
+}
